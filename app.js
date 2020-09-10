a5cad7c26417ed4ee8cd4ebcd1c809a6f9f06798
coding404life <kingwar66@gmail.com>
2020-09-10 20:28:29 +0200

    added items to the ui

--- app.js
+++ app.js
@@ -69,7 +69,9 @@ const UIController = (function () {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
     }
 
     //methods that is exposed to the public
@@ -83,6 +85,31 @@ const UIController = (function () {
             };
 
         },
+
+        addListItem: function (obj, type) {
+            let html, newHtml, element;
+
+            //create html string with placeholder text
+            if (type === 'inc') {
+                element = DOMstring.incomeContainer;
+
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            } else if (type === 'exp') {
+                element = DOMstring.expensesContainer;
+
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+
+            // Replace the placeholder text with some actual data 
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value)
+
+            // insert the html into te DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+        },
+
+
         getDOMstring: function () {
             //expose the DOMstring into the public
             return DOMstring;
@@ -119,7 +146,7 @@ const controller = (function (budgetCtrl, UICtrl) {
         //2- add the new item to our data structure
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         //3- add the new items to the ui
-
+        UICtrl.addListItem(newItem, input.type);
         //4- calculate the new budget 
 
         //5- update the ui with the new budget
